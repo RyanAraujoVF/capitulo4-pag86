d62a3b9f87d79272d005eaa7ceb520c5a1513a80
RyanAraujoVF <153670118+RyanAraujoVF@users.noreply.github.com>
2023-12-18 23:14:19 -0300

    PontosDeParabolaQuestão8.java

--- PontosDeParabolaQuestão8.java
+++ PontosDeParabolaQuestão8.java
@@ -1,20 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package pontosdeparabola.questão8;
 
-/**
- *
- * @author Sandro3
- */
+import javax.swing.JOptionPane;
+
 public class PontosDeParabolaQuestão8 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {     
+        double x, y;
+               
+        x = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor de x:"));
+        y = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor de y:"));
+             
+        if (y == Math.pow(x, 2) + 1) 
+        {
+            JOptionPane.showMessageDialog(null, "O ponto (" + x + ", " + y + ") pertence à parábola y = x^2 + 1");
+        } 
+        else if (y > Math.pow(x, 2) + 1) 
+        {
+            JOptionPane.showMessageDialog(null, "O ponto (" + x + ", " + y + ") está acima da parábola y = x^2 + 1");
+        } 
+        else 
+        {
+            JOptionPane.showMessageDialog(null, "O ponto (" + x + ", " + y + ") está abaixo da parábola y = x^2 + 1");
+        }
+    }
 }
