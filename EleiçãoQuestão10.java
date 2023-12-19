0561d3cca515d6331b303e9a497ebcd020e15fad
RyanAraujoVF <153670118+RyanAraujoVF@users.noreply.github.com>
2023-12-18 23:15:27 -0300

    Update EleiçãoQuestão10.java

--- EleiçãoQuestão10.java
+++ EleiçãoQuestão10.java
@@ -1,20 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package eleição.questão10;
 
-/**
- *
- * @author Sandro3
- */
+import javax.swing.JOptionPane;
+
+
 public class EleiçãoQuestão10 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
+    public static void main(String[] args) {
+   
+        String nome1, nome2, nome3;
+        int votos1, votos2, votos3;
+        
+        nome1 = JOptionPane.showInputDialog("Digite o nome do primeiro candidato:");
+        votos1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o total de votos do primeiro candidato:"));
+        nome2 = JOptionPane.showInputDialog("Digite o nome do segundo candidato:");
+        votos2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o total de votos do segundo candidato:"));
+        nome3 = JOptionPane.showInputDialog("Digite o nome do terceiro candidato:");
+        votos3 = Integer.parseInt(JOptionPane.showInputDialog("Digite o total de votos do terceiro candidato:"));
+        
+        if (votos1 > votos2 && votos1 > votos3) 
+        {
+            JOptionPane.showMessageDialog(null, "O vencedor da eleição foi " + nome1 + " com " + votos1 + " votos.");
+        } 
+        else if (votos2 > votos1 && votos2 > votos3) 
+        {
+            JOptionPane.showMessageDialog(null, "O vencedor da eleição foi " + nome2 + " com " + votos2 + " votos.");
+        } 
+        else if (votos3 > votos1 && votos3 > votos2) 
+        {
+            JOptionPane.showMessageDialog(null, "O vencedor da eleição foi " + nome3 + " com " + votos3 + " votos.");
+        } 
+        else 
+        {
+            JOptionPane.showMessageDialog(null, "Houve um empate na eleição.");
+        }
+    }
 }
